a7896b25ccd54a2f373377f9db7495038e6efd92
Ryan Anklam <ryan.anklam@gmail.com>
2014-05-02 22:56:44 -0700

    made progress on slides

--- cfobjective/2014/ember-presentation/sample/src/controllers/session-controller.js
+++ cfobjective/2014/ember-presentation/sample/src/controllers/session-controller.js
@@ -3,5 +3,8 @@ App.SessionController = Ember.ObjectController.extend({
     going : function( session ){
       this.set('going', ( this.get( 'going' ) ) ? false : true);
     }
-  }
+  },
+  goingClass : function(){
+  	return (this.get( 'going' ))? 'attending' : '';
+  }.property( 'going' )
 });
